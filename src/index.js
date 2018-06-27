74ca198ce7cec5d6e384d65ca3c0dba6794308ca
volker <vmoebius@gmail.com>
2018-06-27 21:35:20 +0200

    minor: started some JSDoc (to be completed)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 'use strict';
 
+/**
+ * @typedef {object} TreeNode
+ * @property {string} type
+ */
+
+/**
+ * @typedef {TreeNode} Combination
+ * @property {string} operator
+ * @property {*} lhs
+ * @property {*} rhs
+ */
+
+/**
+ * @typedef {TreeNode} Constraint
+ * @property {string} selector
+ * @property {string} comparison
+ * @property {*} argument
+ */
+
 const parser = require('./parser');
 const constants = require('./constants');
 
